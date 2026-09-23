f04d7dea8c7933cb431e54b466f1c78309118ae0
AzizBouzouf <jonathandorn20@gmail.com>
2026-09-23 21:12:56 +0200

    Add files via upload

---
+++ altas_loading/addons.js
@@ -0,0 +1,9 @@
+// Genere par update-addons.ps1 - collection 3773975035 - 2026-09-23 21:10
+var WORKSHOP_ADDONS = [
+  { id: "1907060869", title: "Srlion's Hook Library", size: 14008 },
+  { id: "3775083231", title: "Nexux #1", size: 1053014 },
+  { id: "2028581413", title: "RP_SouthSide_Day", size: 980877232 },
+  { id: "105106699", title: "Military Tablet (Outdated)", size: 4761919 },
+  { id: "207948202", title: "Simple ThirdPerson", size: 49069 },
+  { id: "3788644840", title: "Nexux #2", size: 618729 }
+];
